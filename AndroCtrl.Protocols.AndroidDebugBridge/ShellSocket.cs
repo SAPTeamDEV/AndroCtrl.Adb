e065410540e194a6d99b4dea4b4e2672f76cc848
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-02-19 23:30:30 +0330

    Add blocking feature to ShellSocket.ReadAvailable()

--- AndroCtrl.Protocols.AndroidDebugBridge/ShellSocket.cs
+++ AndroCtrl.Protocols.AndroidDebugBridge/ShellSocket.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AndroCtrl.Protocols.AndroidDebugBridge
@@ -21,16 +22,32 @@ namespace AndroCtrl.Protocols.AndroidDebugBridge
         /// <summary>
         /// Reads all available data and converts it to string.
         /// </summary>
-        /// <returns></returns>
-        public string ReadAvailable()
+        /// <param name="blocking">
+        /// determines wait for receiving data from socket.
+        /// </param>
+        /// <returns>
+        /// a string created from read bytes.
+        /// </returns>
+        public string ReadAvailable(bool blocking)
         {
-            int count = Socket.Available;
-
-            if (count > 0)
+            while (true)
             {
-                var resp = new byte[count];
-                Socket.Read(resp);
-                return Encoding.ASCII.GetString(resp);
+                int count = Socket.Available;
+
+                if (count > 0)
+                {
+                    var resp = new byte[count];
+                    Socket.Read(resp);
+                    return Encoding.ASCII.GetString(resp);
+                }
+                else if (blocking)
+                {
+                    Thread.Sleep(1);
+                }
+                else
+                {
+                    break;
+                }
             }
 
             return string.Empty;
