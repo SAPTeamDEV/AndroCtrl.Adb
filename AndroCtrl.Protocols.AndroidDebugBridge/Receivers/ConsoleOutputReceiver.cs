f70a5535f804b9f064cdcc84bf27bdd185c83a0d
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-03-17 12:08:00 +0330

    bugfix

--- AndroCtrl.Protocols.AndroidDebugBridge/Receivers/ConsoleOutputReceiver.cs
+++ AndroCtrl.Protocols.AndroidDebugBridge/Receivers/ConsoleOutputReceiver.cs
@@ -66,7 +66,7 @@ public class ConsoleOutputReceiver : MultiLineReceiver
     /// </param>
     public void ThrowOnError(string line)
     {
-        if (!ParsesErrors)
+        if (ParsesErrors)
         {
             if (line.EndsWith(": not found"))
             {
