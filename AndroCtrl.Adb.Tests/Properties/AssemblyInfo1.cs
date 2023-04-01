e5c5873dd6ca1dd7c7941121bb839a68533a4931
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-04-02 00:41:03 +0330

    Change folder and project properties to match new namespace

---
+++ AndroCtrl.Adb.Tests/Properties/AssemblyInfo1.cs
@@ -0,0 +1,4 @@
+using System.Runtime.InteropServices;
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+[assembly: Guid("611f78fe-0654-4e31-af31-f6ac4760de9a")]
--- appveyor-testresults.ps1
+++ appveyor-testresults.ps1
@@ -1,3 +1,3 @@
 # upload results to AppVeyor
 $wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", (Join-Path $env:APPVEYOR_BUILD_FOLDER .\AndroCtrl.Protocols.AndroidDebugBridge.Tests\TestResults\testresults.trx))
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", (Join-Path $env:APPVEYOR_BUILD_FOLDER .\AndroCtrl.Adb.Tests\TestResults\testresults.trx))
