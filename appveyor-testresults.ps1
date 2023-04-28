68babe4e36914dd5a139e074f54cfdf72c9d1c67
Ali-p1986 <131697486+Ali-p1986@users.noreply.github.com>
2023-04-28 09:52:27 +0330

    Change directory structure

--- appveyor-testresults.ps1
+++
@@ -1,3 +0,0 @@
-# upload results to AppVeyor
-$wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", (Join-Path $env:APPVEYOR_BUILD_FOLDER .\AndroCtrl.Adb.Tests\TestResults\testresults.trx))
